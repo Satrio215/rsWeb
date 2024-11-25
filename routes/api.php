638abb3ad30450db174034edac69a3db0deee789
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2024-11-25 19:56:19 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppointmentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+
+
+
+//fitur appointments
+Route::get('/appointments', [AppointmentController::class, 'coba']);
+Route::get('/appointments/show', [AppointmentController::class, 'index']);
+Route::get('/appointments/showselect', [AppointmentController::class, 'indexselection']);
