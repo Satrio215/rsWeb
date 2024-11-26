622c41dcd3e36b13df99aa1213261c3551202a8e
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2024-11-26 21:58:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/', [ProfileController::class, 'edit']);
+
+
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
